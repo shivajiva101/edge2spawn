eb222155e6434583cc2dd64a68b6e7756432114d
shivajiva101 <shivajiva101@hotmail.com>
2016-05-12 16:38:02 +0100

    Update init.lua
    code cleanup

--- init.lua
+++ init.lua
@@ -6,11 +6,11 @@ local edge = tonumber(minetest.setting_get("edge2spawn_edge")) or 1000
 local spawn = minetest.setting_get_pos("static_spawnpoint") or {x= 0, y= 0, z=0}
 
 -- register priv
-minetest.register_privilege("explorer","Player can move beyond the edge")
+minetest.register_privilege("explorer","Player can move beyond the edge limit")
 
 --[[ Function creates a table of players and loop through checking players positions
 against the edge value, returning any players exceeding this value to
-the static spawnpoint ]]  
+the static spawnpoint unless they have the 'explorer' privilege ]]  
 function checkPlayers()
 -- create table
 local players = minetest.get_connected_players()
@@ -18,19 +18,20 @@ local players = minetest.get_connected_players()
   for i, player in ipairs(players) do
 	-- get player position
 	local pos = vector.round(player:getpos())
-	-- check player isn't beyond the edge
-	if player then  
-	  if not minetest.check_player_privs(player:get_player_name(), { explorer = true }) then
+	-- get player name
+	local playerName = player:get_player_name()
+	-- check player priv
+	  if not minetest.check_player_privs(playerName, { explorer = true }) then
+	  	-- check player position
 	    if pos.x >= edge or pos.z >= edge or pos.x <= -edge or pos.z <= -edge then
 		player:setpos(spawn) -- move player to spawn
-		minetest.chat_send_player(player:get_player_name(), "Oops you're not allowed to go that far!")
+		minetest.chat_send_player(playerName, "Oops you're not allowed to go that far!")
 	    end	
 	  end
-	end
   end
 end
 
--- register 
+-- register global step
 local stepTime = 0
 minetest.register_globalstep(function(dtime)
 	stepTime = stepTime + dtime
